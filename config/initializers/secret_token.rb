80e5fec7ae9df6b8ad2d6f6f687388114903a5aa
Rodolfo Maldonado <mckain.default@gmail.com>
2013-07-04 11:05:25 -0600

    Initial commit Yay! :)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SinkIt::Application.config.secret_token = 'd28e08e98928415dd0bcb35795b7e0bc0a19de43806ab58ff65e7ff32aaf68a7c72c01b87c207f95096d7c62810ccb8e3815dfdc0302150614802f11dfbbe785'
