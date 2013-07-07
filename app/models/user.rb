0a59866a4585193594e059af3a27cbbd703e4e31
Ed Mckain <mckain.default@gmail.com>
2013-07-06 21:02:45 -0600

    adds name to devise + generates devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,9 @@ class User
   field :current_sign_in_ip, :type => String
   field :last_sign_in_ip,    :type => String
 
+  ## Edited by mcka1n
+  field :name,    :type => String  
+
   ## Confirmable
   # field :confirmation_token,   :type => String
   # field :confirmed_at,         :type => Time
