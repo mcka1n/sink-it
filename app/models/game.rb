e65c044e1e516c8fa4a05e4d1a93ff6265f60f92
Ed Mckain <mckain.default@gmail.com>
2013-07-06 20:16:02 -0600

    improves Game Model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,10 @@
 class Game
   include Mongoid::Document
 
+  field :user_id, :type => String
+  field :game_status, :type => String, :default => "playing"
+  field :prize, :type => String, :default => ""
+
+  belongs_to :user
+
 end
